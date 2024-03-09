f6c4e9f9891c2b7f2f837fb517751a0c7a54b918
guDutra <gudutra05@outlook.com>
2024-03-08 21:56:30 -0300

    iniciando projeto e criando sidebar

---
+++ AdminFinanceiro/financias/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Navbar from './components/navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home'
+import Reports from './pages/Reports'
+import Products from './pages/Products'
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact Component={Home} />
+          <Route path='/reports' Component={Reports} />
+          <Route path='/products' Component={Products} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
