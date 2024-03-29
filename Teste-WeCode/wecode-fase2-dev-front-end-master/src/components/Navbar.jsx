a05e204dea4a9a4f03fc358ef626533341b60f7e
guDutra <gudutra05@outlook.com>
2024-03-29 00:53:36 -0300

    criado scrool troca navbar sem img

--- Teste-WeCode/wecode-fase2-dev-front-end-master/src/components/Navbar.jsx
+++ Teste-WeCode/wecode-fase2-dev-front-end-master/src/components/Navbar.jsx
@@ -1,25 +1,44 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import * as FaIcons from "react-icons/fa"
 import * as AiIcons from "react-icons/ai"
 import * as IoIcons from "react-icons/io"
 import * as MdIcons from "react-icons/md"
 import '../components/navbar.scss'
-import { IconContext } from 'react-icons'
+
 const Navbar = () => {
+
+    const [navbarBackground, setNavbarBackground] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const isNavbarBackground = window.scrollY > 0;
+            setNavbarBackground(isNavbarBackground);
+            
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
         <div className='navbar'>
+            <nav className={`${navbarBackground ? 'navbar-white' : ''}`}>
+                <div className='icons-left'>
+                    <i><FaIcons.FaBars /></i>
+                    <i><FaIcons.FaSearch /> </i>
+                </div>
+                <div className='logo-name'>
+                    <img src={`${navbarBackground ? '/static/images/principais/logo-preta.png' : '/static/images/principais/Logo.png'} `} alt="logo" />
+                </div>
+                <div className='icons-right'>
+                    <i><MdIcons.MdPerson /></i>
+                    <i><AiIcons.AiOutlineShopping /> </i>
+                </div>
+            </nav>
 
-            <div className='icons-left'>
-                <i><FaIcons.FaBars /></i>
-                <i><FaIcons.FaSearch /> </i>
-            </div>
-            <div className='logo-name'>
-                <img src="/static/images/principais/logo-preta.png" alt="logo" />
-            </div>
-            <div className='icons-right'>
-                <i><MdIcons.MdPerson /></i>
-                <i><AiIcons.AiOutlineShopping /> </i>
-            </div>
 
         </div>
     )
