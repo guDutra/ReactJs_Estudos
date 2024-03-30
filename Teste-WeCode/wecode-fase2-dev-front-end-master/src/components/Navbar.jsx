dd6a4dd8ff43e8260bcad6b3575aeecf79557550
guDutra <gudutra05@outlook.com>
2024-03-30 15:20:18 -0300

    tudo em um componente antes de editar navbar para contar produtos

--- Teste-WeCode/wecode-fase2-dev-front-end-master/src/components/Navbar.jsx
+++ Teste-WeCode/wecode-fase2-dev-front-end-master/src/components/Navbar.jsx
@@ -1,10 +1,9 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import * as FaIcons from "react-icons/fa"
 import * as AiIcons from "react-icons/ai"
 import * as IoIcons from "react-icons/io"
 import * as MdIcons from "react-icons/md"
 import '../components/navbar.scss'
-
 const Navbar = () => {
 
     const [navbarBackground, setNavbarBackground] = useState(false);
@@ -13,7 +12,7 @@ const Navbar = () => {
         const handleScroll = () => {
             const isNavbarBackground = window.scrollY > 0;
             setNavbarBackground(isNavbarBackground);
-            
+
         };
 
         window.addEventListener('scroll', handleScroll);
@@ -22,7 +21,6 @@ const Navbar = () => {
             window.removeEventListener('scroll', handleScroll);
         };
     }, []);
-
     return (
         <div className='navbar'>
             <nav className={`${navbarBackground ? 'navbar-white' : ''}`}>
@@ -31,15 +29,13 @@ const Navbar = () => {
                     <i><FaIcons.FaSearch /> </i>
                 </div>
                 <div className='logo-name'>
-                    <img src={`${navbarBackground ? '/static/images/principais/logo-preta.png' : '/static/images/principais/Logo.png'} `} alt="logo" />
+                    <img src="/static/images/principais/logo-preta.png" alt="logo" />
                 </div>
                 <div className='icons-right'>
                     <i><MdIcons.MdPerson /></i>
                     <i><AiIcons.AiOutlineShopping /> </i>
                 </div>
             </nav>
-
-
         </div>
     )
 }
